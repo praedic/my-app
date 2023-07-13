db43585bed4e40dab62449f245f6f8219caf5d35
praedic <grupo.praedic@gmail.com>
2023-07-13 19:13:17 -0300

    teste2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,55 @@ export default function Home() {
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
+        </p>import { useState } from 'react';
+import Link from 'next/link';
+
+export default function Home({ carros }) {
+  const [selecionados, setSelecionados] = useState([]);
+
+  const toggleSelecao = (carro) => {
+    const jaSelecionado = selecionados.includes(carro);
+    setSelecionados(jaSelecionado ? selecionados.filter(c => c !== carro) : [...selecionados, carro]);
+  };
+
+  return (
+    <div>
+      <h1>Selecione suas marcas e modelos favoritos</h1>
+      {carros.map((carro) => (
+        <div key={carro.id}>
+          <label>
+            <input type="checkbox" checked={selecionados.includes(carro)} onChange={() => toggleSelecao(carro)} />
+            {carro.marca} {carro.modelo}
+          </label>
+        </div>
+      ))}
+      <Link href={{ pathname: '/recomendacoes', query: { carros: selecionados.map(c => c.id) } }}>
+        <a>Ver recomendações</a>
+      </Link>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  // Substitua com a lógica de busca dos carros do seu backend.
+  // const res = await fetch('http://seu-backend/carros');
+  // const carros = await res.json();
+
+  // Lista estática de carros
+  const carros = [
+    { id: 1, marca: 'Marca 1', modelo: 'Modelo 1' },
+    { id: 2, marca: 'Marca 2', modelo: 'Modelo 2' },
+    { id: 3, marca: 'Marca 3', modelo: 'Modelo 3' },
+    // Adicione mais carros conforme necessário
+  ];
+
+  return {
+    props: {
+      carros,
+    },
+  };
+}
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
